ebc91f12245b8c819ccf447e47776ac1f1242eb8
Joanna-Joy Gray <jjgray21@hotmail.com>
2020-11-19 18:55:06 -0500

    PHP accepts get request variables

--- world.php
+++ world.php
@@ -1,7 +1,9 @@
 <?php
 $host = 'localhost';
 $username = 'lab5_user';
-$password = '';
+// $username = root;
+$password = 'password123';
+//$password = '';
 $dbname = 'world';
 
 $conn = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $password);
@@ -10,8 +12,26 @@ $stmt = $conn->query("SELECT * FROM countries");
 $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
 ?>
-<ul>
+<!-- <ul>
 <?php foreach ($results as $row): ?>
   <li><?= $row['name'] . ' is ruled by ' . $row['head_of_state']; ?></li>
 <?php endforeach; ?>
-</ul>
+</ul> -->
+
+
+<?php
+if ($_SERVER['REQUEST_METHOD'] == 'GET'){
+  if(isset($_GET['country']) && !empty($_GET['country'])){
+    $country = filter_input(INPUT_GET, 'country', FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+    $found = false;
+    $stmt = $conn->query("SELECT * FROM countries WHERE name like '%$country%'");
+    $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+    ?>
+    <ul>
+    <?php foreach ($results as $row): ?>
+      <li><?= $row['name'] . ' is ruled by ' . $row['head_of_state']; ?></li>
+    <?php endforeach;?>
+    </ul>
+    <?php
+  }
+}
